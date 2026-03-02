556bbf8e2f16fbbcde009ca92730347cc7df095e
Dugger8807 <admin@duggerlink.com>
2026-03-01 21:06:28 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { theme } from "./constants.js";
 import { Icon, Btn } from "./ui.jsx";
 import { useIsMobile } from "./useMediaQuery.js";
